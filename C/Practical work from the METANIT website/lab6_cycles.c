255679bcc7484f47622dc5e9c333309818c6900f
Sergey <s.efimov1904@gmail.com>
2025-02-25 01:17:09 +0300

    Create lab6_cycles.c

---
+++ C/Practical work from the METANIT website/lab6_cycles.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Task 1
+
+    for (int i = 1; i <=5; i++)
+    {
+        printf("%d. Hello Work! \n", i);
+    }
+
+    // Task 2
+
+    for (int i = 10; i < 20; i++)
+    {
+        if (i % 2 == 1)
+        {
+            printf("%d \n", i);
+        }
+    }
+
+    return 0;
+}
