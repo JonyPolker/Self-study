e13a5a1356b49d8e5d29b10132cb4c54be09b104
Sergey <s.efimov1904@gmail.com>
2025-02-26 12:02:22 +0300

    Update lab7_introduction_to_arrays_and_strings.c

--- C/Practical work from the METANIT website/lab7_introduction_to_arrays_and_strings.c
+++ C/Practical work from the METANIT website/lab7_introduction_to_arrays_and_strings.c
@@ -6,7 +6,8 @@ int main(void)
 
     char str[] = {"Random tetx"};
     printf("Text is \"%s\" \n", str);
-
+    
+    printf("\n");
     // Task 2
 
     int arr[4] = {1, 2, 3, 4};
@@ -17,7 +18,8 @@ int main(void)
     {
         printf("Number %d is %d \n", i+1, arr[i]);
     }
-
+    
+    printf("\n");
     // Task 3
 
     int arr1[4] = {1, 2, 3, 4};
@@ -25,6 +27,23 @@ int main(void)
     {
         arr1[i] *= 2;
         printf("Number %d is %d \n", i+1, arr1[i]);
+    }
+    
+    printf("\n");
+    // Task 4
+    
+    int arr2[5] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr2) / sizeof(arr2[0]);
+    int c = size / 2;
+    for (int i = 0; i < c; i++)
+    {
+        int cache = arr2[i];
+        arr2[i] = arr2[size-i-1];
+        arr2[size-i-1] = cache;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", arr2[i]);
     }
     return 0;
 }
